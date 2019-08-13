d2502a0045166e89187bd56f6c86ec0762968123
Preston Howell <pshowell23@gmail.com>
2019-08-12 21:16:49 -0400

    Finally get the menu to work! Also remove base theme as it is unnecessary

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,25 +1,25 @@
 jQuery(document).ready(function($){
-    $('dd').hide();
-    $('dt').click(function(){
+    //$('dd').hide();
+    //$('dt').click(function(){
         //Expands FAQ Answers
-        $(this).next().slideToggle('slow');
+    //    $(this).next().slideToggle('slow');
 
         //Toggle Plus/Minus icon on FAQ Expansion
-        let icon = $(this).find('.expand-icon').children();
+    //    let icon = $(this).find('.expand-icon').children();
 
-        if(icon.hasClass('fa-plus-circle')){
-            icon.removeClass('fa-plus-circle').addClass('fa-minus-circle');
-        } else {
-            icon.removeClass('fa-minus-circle').addClass('fa-plus-circle');
-        }
-    })
+    //    if(icon.hasClass('fa-plus-circle')){
+    //        icon.removeClass('fa-plus-circle').addClass('fa-minus-circle');
+    //    } else {
+    //        icon.removeClass('fa-minus-circle').addClass('fa-plus-circle');
+    //    }
+    //})
 
     //Check email form and enable submit button when filled out
-    $('.emailForm textarea').keypress(function(){
-        if($('.emailForm #name').val() != '' && $('.emailForm #email').val() != ''){
-            $('.submit-button').prop('disabled', false);
-        }
-    })
+    //$('.emailForm textarea').keypress(function(){
+    //    if($('.emailForm #name').val() != '' && $('.emailForm #email').val() != ''){
+    //        $('.submit-button').prop('disabled', false);
+    //    }
+    //})
 
     //For Mobile navigation
     $(window).resize(function(){
