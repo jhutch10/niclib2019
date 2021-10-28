d0958e098cff142a66d3347906e121859da5d946
jhutch10 <91697531+jhutch10@users.noreply.github.com>
2021-10-28 12:20:59 -0400

    Spanish Home Page Added
    links to Spanish language resources on worldcat, google translate libguide.

--- README.md
+++ README.md
@@ -1,10 +1,11 @@
 # Theme for the Nicholson Library of Anderson University (IN)
 Drupal 9 theme for a 2021 update to website.
+Now featuring Spanish on /es!
 
 ## Built With
 * Bootstrap v4.2.1
 * jQuery v3.4.1
 
-Have to manually load niclib_doors file as the one included is heavily compressed (might not have to anymore)
+Download and unzip folder, place manually into themes.
 
 ## UPDATED TO DRUPAL 9
--- assets/js/searchbox.js
+++ assets/js/searchbox.js
@@ -2,6 +2,10 @@ const BOOKS_AND_MEDIA = '197,239,283,638,1279,1708,1736,1978,2175,2513,3369';
 const BOOKS_AND_MEDIA_FORMAT_STRING = "&format=Book&format=Msscr&format=Jrnl&format=News&format=music&format=Video&format=archv&year=all&yearFrom=&yearTo=&language=all";
 const ARTICLES = '638,1271,1275,1279,1697,1708,1721,1723,1725,1728,1734,1736,1834,1941,1953,1978,2006,2007,2237,2259,2260,2261,2264,2375,3006,3313,3863,3866,3879,3384';
 const ARTICLES_FORMAT_STRING = "&format=Artchap&format=Encyc";
+const SPANISH = "";
+const SPANISH_FORMAT_STRING = "&changedFacet=language&scope=&language=Spa";
+const ES_EBOOKS = "";
+const ES_EBOOKS_FORMAT_STRING = "&subformat=Book%3A%3Abook_digital&scope=wz%3A1274";
 const DISCOVERY_URL = 'https://anderson.on.worldcat.org/v2/search?';
 
 jQuery(document).ready(function($){
@@ -28,6 +32,15 @@ jQuery(document).ready(function($){
 				formats = BOOKS_AND_MEDIA_FORMAT_STRING + ARTICLES_FORMAT_STRING;
                 searchType = 'All Formats Search';
                 break;
+			case 'spanish':
+				databaseList = BOOKS_AND_MEDIA + ',' + ARTICLES;
+				formats = BOOKS_AND_MEDIA_FORMAT_STRING + ARTICLES_FORMAT_STRING + SPANISH_FORMAT_STRING;
+				searchType = 'Spanish Language Only Search';
+				break;
+			case 'es_ebooks':
+				databaseList = BOOKS_AND_MEDIA;
+				formats = SPANISH_FORMAT_STRING + ES_EBOOKS_FORMAT_STRING;
+				searchType = 'AU Only Spanish eBooks'
         }
 		
 		//ga('send', 'event', 'Catalog Search', 'Search', searchType);
